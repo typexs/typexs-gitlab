d7d0622bcb8258d4da9a4f54d718a6d8779804f1
Karl-Philipp Wulfert <krlwlfrt@gmail.com>
2019-05-24 15:30:48 +0200

    fix: fix gulpfile

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,9 +1,9 @@
-import * as _ from "lodash";
 import * as glob from "glob";
+import * as _ from "lodash";
 
 
 _.concat(
-  glob.sync('node_modules/*/gulp/*'),
+  glob.sync('node_modules/@typexs/base/gulp/*.js'),
   glob.sync('src/gulp/*'),
   glob.sync('gulp/*'),
 ).filter(x => !/@types\//.test(x))
