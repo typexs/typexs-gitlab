79de194f12cdfa46bdaa35e1803c2b9461d2e5d3
Robert Kania <cezaryrk@yahoo.de>
2019-11-13 16:25:12 +0100

    build: update gulpfile + version

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,11 +1,9 @@
-import * as glob from "glob";
-import * as _ from "lodash";
-
-
-_.concat(
-  glob.sync('node_modules/@typexs/base/gulp/*.js'),
-  glob.sync('src/gulp/*'),
-  glob.sync('gulp/*'),
-).filter(x => !/@types\//.test(x))
+import * as glob from 'glob';
+[
+  ...glob.sync('node_modules/*/*/gulp/*.js'),
+  ...glob.sync('node_modules/*/gulp/*.js'),
+  ...glob.sync('src/gulp/*'),
+  ...glob.sync('gulp/*'),
+]
+  .filter(x => !/@types\//.test(x))
   .map(x => require('./' + x));
-
